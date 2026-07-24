e414969759f614b195e548bb63a9ecf9f60614da
abhayanigam <abhaya.nigam13@gmail.com>
2026-07-24 23:26:18 +0530

    Added Build File to web deploy

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+echo "flutter_build_script: starting..."
+
+# Install Flutter SDK
+if cd flutter; then
+  git pull && cd ..
+else
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+
+# Add Flutter to PATH
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# Print Flutter version
+flutter --version
+
+# Enable web support (just in case, though usually enabled on stable)
+flutter config --enable-web
+
+# Get dependencies
+flutter pub get
+
+# Build the web project
+# --release for production build
+# --web-renderer html or canvaskit (default is auto, causing canvaskit download)
+# often 'auto' is fine, but sometimes 'html' is preferred for initial load.
+# Keeping default 'auto' for now.
+flutter build web --release
+
+echo "flutter_build_script: build finished"
